df09e2489645a2bfc6717ccd107a8dae69249820
Ruslan Gazihanov <rualnar@gmail.com>
2020-03-13 14:11:32 +0400

    UNIT-6 : Add symbol & for passing value by reference

--- app/addons/comics_info/func.php
+++ app/addons/comics_info/func.php
@@ -4,7 +4,7 @@ if (!defined('BOOTSTRAP')) {
     die('Access denied');
 }
 
-function fn_comics_info_update_product_pre($product_data, $product_id, $lang_code, $can_update) {
+function fn_comics_info_update_product_pre(&$product_data, $product_id, $lang_code, $can_update) {
     if (!empty($product_data['release_date'])) {
         $product_data['release_date'] = fn_parse_date($product_data['release_date']);
     }
