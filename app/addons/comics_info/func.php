240949a1a0358ccecb203b7f150ddfbfa177655b
Ruslan Gazihanov <rualnar@gmail.com>
2020-03-13 13:04:22 +0400

    UNIT-6 : Use update_product_pre hook

---
+++ app/addons/comics_info/func.php
@@ -0,0 +1,11 @@
+<?php
+
+if (!defined('BOOTSTRAP')) {
+    die('Access denied');
+}
+
+function fn_comics_info_update_product_pre($product_data, $product_id, $lang_code, $can_update) {
+    if (!empty($product_data['release_date'])) {
+        $product_data['release_date'] = fn_parse_date($product_data['release_date']);
+    }
+}
